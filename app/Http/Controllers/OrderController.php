d012c7674dbfd23ec773a28fd854227f930ba35a
wilsut <williamonlly@gmail.com>
2019-07-06 09:44:20 +0700

    fix checkout auth

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,7 +10,7 @@ class OrderController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('create');
+        $this->middleware('auth');
     }
 
     /**
